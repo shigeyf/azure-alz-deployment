7d63d6305f35567c1b523601d15fd8a1d5dd199c
Shige Fukushima <shigeyf@outlook.com>
2025-03-29 01:36:26 +0900

    fix: 🐛 Added vnet_name in outputs

--- deployments/terraform/modules/private_vnet/outputs.tf
+++ deployments/terraform/modules/private_vnet/outputs.tf
@@ -2,6 +2,7 @@
 
 output "private_vnet" {
   value = {
+    vnet_name  = azurerm_virtual_network.vnet.name
     vnet_id    = azurerm_virtual_network.vnet.id
     subnet_ids = { for key, subnet in azurerm_subnet.snet : key => subnet.id }
     nsg_ids    = { for key, nsg in azurerm_network_security_group.nsg : key => nsg.id }

2dcf80c5ded55bb7a0e31a9ce3c835d82099d6b5
Shige Fukushima <shigeyf@outlook.com>
2025-03-31 02:40:17 +0900

    fix: 🐛 Added role assignment output

--- deployments/terraform/modules/private_keyvault/outputs.tf
+++ deployments/terraform/modules/private_keyvault/outputs.tf
@@ -2,8 +2,9 @@
 
 output "private_keyvault" {
   value = {
-    keyvault_id    = azurerm_key_vault.keyvault.id
-    keyvault_pe_id = azurerm_private_endpoint.keyvault.id
+    keyvault_id      = azurerm_key_vault.keyvault.id
+    keyvault_pe_id   = azurerm_private_endpoint.keyvault.id,
+    role_assignments = azurerm_role_assignment.ra,
   }
   description = "Ids for KeyVault resources"
 }

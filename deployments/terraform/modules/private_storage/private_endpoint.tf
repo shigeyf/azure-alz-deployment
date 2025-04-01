25c7192361c102d967f304de6a4408bb98b97b3e
Shige Fukushima <shigeyf@outlook.com>
2025-04-02 03:54:14 +0900

    fix: 🐛 Added private dns zone parameter

--- deployments/terraform/modules/private_storage/private_endpoint.tf
+++ deployments/terraform/modules/private_storage/private_endpoint.tf
@@ -4,6 +4,7 @@ resource "azurerm_private_endpoint" "storage" {
   name                = local.storage_pe_name
   location            = var.location
   resource_group_name = var.resource_group_name
+  tags                = var.tags
   subnet_id           = var.private_endpoint_subnet_id
 
   private_service_connection {
@@ -11,6 +12,11 @@ resource "azurerm_private_endpoint" "storage" {
     private_connection_resource_id = azurerm_storage_account.storage.id
     is_manual_connection           = false
     subresource_names              = ["Blob"]
+  }
+
+  private_dns_zone_group {
+    name                 = "storage-dns-zone-group"
+    private_dns_zone_ids = var.private_dns_zone_ids
   }
 
   depends_on = [
--- deployments/terraform/modules/private_storage/variables.tf
+++ deployments/terraform/modules/private_storage/variables.tf
@@ -41,6 +41,11 @@ variable "private_endpoint_subnet_id" {
   description = "Private Endpoint Subnet Id for Storage account"
 }
 
+variable "private_dns_zone_ids" {
+  type        = list(string)
+  description = "List of Private DNS Zone Id for Storage account"
+}
+
 variable "keyvault_id" {
   type        = string
   description = "Key Vault Id"

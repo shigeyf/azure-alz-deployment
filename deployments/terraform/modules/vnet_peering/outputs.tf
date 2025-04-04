dc0475bc86aafdd1c48a5a0aab1267e8dee70665
Shige Fukushima <shigeyf@outlook.com>
2025-04-04 21:54:05 +0900

    fix: 🐛 Fixed format

--- deployments/terraform/modules/vnet_peering/outputs.tf
+++ deployments/terraform/modules/vnet_peering/outputs.tf
@@ -6,6 +6,11 @@ locals {
 }
 
 output "vnet_peerings" {
-  value       = { for i in range(length(local.from)) : i => { local.from[i] : azurerm_virtual_network_peering.from_to_peering[local.from[i]].id, local.to[i] : azurerm_virtual_network_peering.to_from_peering[local.to[i]].id } }
+  value = {
+    for i in range(length(local.from)) : i => {
+      local.from[i] : azurerm_virtual_network_peering.from_to_peering[local.from[i]].id,
+      local.to[i] : azurerm_virtual_network_peering.to_from_peering[local.to[i]].id,
+    }
+  }
   description = "List of VNet peering pairs"
 }
